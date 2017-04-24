50fa2d4e3198d5a9219cccf3c6c5e7af4d4f0bf4
saviourgidi <profsaviour@gmail.com>
2017-04-24 16:53:32 +0000

    learning curves

--- hapi-handlebar/server.js
+++ hapi-handlebar/server.js
@@ -1,45 +1,44 @@
-const hapi = require('hapi');
-
+const Hapi = require('hapi');
+// const handlerbars = require('handlebars');
 // Create server connection instance
-const server = new hapi.server();
+const server = new Hapi.Server();
+server.connection({ port: 3000 });
 
-server.connection({ 
-    // host: '127.0.0.1',
-    port: 3000
+const handler = function(request, reply){
+
+};
+
+server.register(require('vision'), (err) => {
+  if(err){
+    console.log(err);
+  }
+  // view engine [handlebars]
+  server.views({
+      engines: {
+          html: require('handlebars')
+      },
+      path: './views',
+      layoutPath: './views/layout',
+      layout: 'default',
+      helpersPath: './views/helpers',
+      partialsPath: './views/partials'
+  });
+
+  server.route({
+          method: 'GET',
+          path: '/',
+          handler: function(request, reply){
+          var data = {
+              title: 'This is index',
+              message: 'Hello world, handlebars things'
+          };
+
+          return reply.view('index', data);
+          }
+      });
+
 });
 
-
-// view engine [handlebars]
-server.views({
-    engines: {
-        html: require('handlebars');
-    },
-    path: 'views',
-    layoutPath: 'views/layout',
-    layout: 'default',
-    //helpersPath: 'views/helpers',
-    //partialsPath: 'views/partials'
-});
-
-var routes = [
-    {
-        method: 'GET',
-        path: '/',
-        handler: function(request, reply){
-        var data = {
-            title: 'This is index',
-            message: 'Hello world, handlebars things'
-        };
-
-        return reply.view('index', data);
-        }
-    }
-];
-
-
-server.route(routes);
 server.start(function(){
     console.log('server started at: ' + server.info.uri);
 });
-
-
